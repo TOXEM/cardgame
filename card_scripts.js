dc18e5f71dd599425e35c18c0561ccf4f8132f5c
TOXEM <157594440+TOXEM@users.noreply.github.com>
2024-02-15 15:42:24 +0300

    Add files via upload

--- card_scripts.js
+++ card_scripts.js
@@ -1,13 +1,30 @@
-const elements = document.querySelectorAll('.card');
-const drop = document.querySelector('.player-zone');
+const elements = document.querySelectorAll(".card");
+const drop = document.querySelector(".player-zone");
 
-elements.forEach(function(element) {
-    element.addEventListener('mousedown', function(event) {
-        const hoveredElement = event.target;
-        hoveredElement.classList.add('player-zone');
-        hoveredElement.classList.add('card');
-        drop.appendChild(hoveredElement);
-        console.log(hoveredElement.alt);
-    });
+// перенос карты в зону игрока при одном нажатии ЛКМ
+elements.forEach(function (element) {
+  element.addEventListener("mousedown", function (event) {
+    const portableElement = event.target.parentNode; //присваиваем переменной контейнер на который кликнули ЛКМ
+    const elemID = "#pz_" + portableElement.parentNode.id; //вычитываем ID из перемещаемого элемента
+    const drop_container = document.querySelector(elemID); //присваиваем переменной соответсвующий ID контейнера
+    drop_container.appendChild(portableElement); //добавляем в игровую зону карту
+  });
+
+  // увеличение карты в 1.5 раза при наведении курсора
+  element.addEventListener("mouseover", function (event) {
+    const largeIMG = event.target;
+    largeIMG.style.transform = "scale(1.4)";
+    largeIMG.style.transformOrigin = "center";
+    largeIMG.style.position = "relative";
+    largeIMG.style.zIndex = "1000";
+  });
+
+  // возврат размера в исходное состояние
+  element.addEventListener("mouseout", function (event) {
+    const largeIMG = event.target;
+    largeIMG.style.transform = "";
+    largeIMG.style.transformOrigin = "";
+    largeIMG.style.position = "";
+    largeIMG.style.zIndex = "";
+  });
 });
-
